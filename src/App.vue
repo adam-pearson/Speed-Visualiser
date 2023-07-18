fbb286b5b19fbed014cd727e6b7ad40b5b41ddba
Adam Pearson <adam.pearson94@gmail.com>
2023-07-18 09:30:11 +0100

    Initial setup
    Set up vue ts w/ tailwind

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <p class="text-3xl text-gray-700 font-bold mb-5">
+      Welcome!
+    </p>
+    <p class="text-gray-500 text-lg">
+      Vue and Tailwind CSS in action
+    </p>
+ </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
